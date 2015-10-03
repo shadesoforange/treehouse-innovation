d989f70e7576c9ab57f855df86f94934286ea76b
Ian Monk <ian@creativedepartment.com>
2015-10-03 15:31:07 -0400

    updating

--- sites/all/themes/treehouse/js/script.js
+++ sites/all/themes/treehouse/js/script.js
@@ -28,6 +28,21 @@ Drupal.behaviors.my_custom_behavior = {
 	
 	$('.accordion').accordion({header:'.header',active: 'false'});
 	
+	$(window).scroll(function () {
+		if ($(this).scrollTop() > 100) {
+			$('.back-to-top').fadeIn();
+		} else {
+			$('.back-to-top').fadeOut();
+		}
+	});
+	
+	$('.back-to-top a').click(function () {
+		$('body,html').animate({
+			scrollTop: 0
+		}, 800);
+		return false;
+	});
+		
   }
 };
 
